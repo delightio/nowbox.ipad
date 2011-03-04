5d5aad087ca301eaa89296df1e1a2f0f60b4a71d
Bill So <hokchun@gmail.com>
2011-03-04 15:42:26 +0800

    application foundation

---
+++ NMLibrary/Tasks/NMGetVimeoDirectURL.m
@@ -0,0 +1,14 @@
+//
+//  NMGetVimeoDirectURL.m
+//  Nowmov
+//
+//  Created by Bill So on 04/03/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "NMGetVimeoDirectURL.h"
+
+
+@implementation NMGetVimeoDirectURL
+
+@end
---
+++ NMLibrary/Tasks/NMGetYouTubeDirectURL.m
@@ -0,0 +1,14 @@
+//
+//  NMGetYouTubeDirectURL.m
+//  Nowmov
+//
+//  Created by Bill So on 04/03/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "NMGetYouTubeDirectURL.h"
+
+
+@implementation NMGetYouTubeDirectURL
+
+@end
