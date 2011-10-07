74c603cc000e9103a4ad3661fca09816ad22c699
Bill So <hokchun@gmail.com>
2011-10-08 03:11:32 +0800

    Twitter/Facebook login

--- Classes/TwitterLoginViewController.h
+++ Classes/TwitterLoginViewController.h
@@ -8,14 +8,21 @@
 
 #import <UIKit/UIKit.h>
 
+typedef enum {
+	LoginTwitterType,
+	LoginFacebookType,
+} SocialLoginType;
+
 @interface TwitterLoginViewController : UIViewController <UIWebViewDelegate> {
 	IBOutlet UIActivityIndicatorView * loadingIndicator;
 	IBOutlet UILabel * progressLabel;
 	UIWebView *loginWebView;
 	UIView * progressContainerView;
+	SocialLoginType loginType;
 }
 
 @property (retain, nonatomic) IBOutlet UIWebView *loginWebView;
 @property (nonatomic, retain) IBOutlet UIView * progressContainerView;
+@property (nonatomic, assign) SocialLoginType loginType;
 
 @end
--- Classes/TwitterLoginViewController.m
+++ Classes/TwitterLoginViewController.m
@@ -12,6 +12,7 @@
 
 @implementation TwitterLoginViewController
 @synthesize loginWebView, progressContainerView;
+@synthesize loginType;
 
 //- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 //{
@@ -43,10 +44,24 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	NSString * urlStr = [NSString stringWithFormat:@"http://api.nowbox.com/auth/twitter?user_id=%d", NM_USER_ACCOUNT_ID];
-	[loginWebView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:urlStr] cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:30.0f]];
 	
-	self.title = @"Twitter Login";
+	NSString * filename = nil;
+	switch (loginType) {
+		case LoginTwitterType:
+			self.title = @"Twitter";
+			filename = @"TwitterLoading";
+			break;
+			
+		case LoginFacebookType:
+			self.title = @"Facebook";
+			filename = @"FacebookLoading";
+			break;
+			
+		default:
+			break;
+	}
+	NSURL * theURL = [[NSBundle mainBundle] URLForResource:filename withExtension:@"html"];
+	[loginWebView loadRequest:[NSURLRequest requestWithURL:theURL]];
 	
 	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleSocialMediaLoginNotificaiton:) name:NMDidVerifyUserNotification object:nil];
 }
@@ -57,6 +72,25 @@
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+	[super viewDidAppear:animated];
+	NSString * urlStr = nil;
+	switch (loginType) {
+		case LoginTwitterType:
+			urlStr = [NSString stringWithFormat:@"http://api.nowbox.com/auth/twitter?user_id=%d", NM_USER_ACCOUNT_ID];
+			break;
+			
+		case LoginFacebookType:
+			urlStr = @"http://api.nowbox.com/auth/facebook";
+			break;
+			
+		default:
+			break;
+	}
+	
+	[loginWebView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:urlStr] cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:30.0f]];
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
@@ -86,29 +120,44 @@
 	
 	progressLabel.text = @"Verified Successfully";
 	[loadingIndicator stopAnimating];
-	[self performSelector:@selector(delayPushOutView) withObject:nil afterDelay:2.0f];
+	[self performSelector:@selector(delayPushOutView) withObject:nil afterDelay:1.5f];
 }
 
 #pragma mark Webview delegate methods
 - (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
 	NSURL * theURL = [request URL];
-	NSLog(@"Twitter URL: %@", theURL);
-	if ( [[theURL path] isEqualToString:@"/auth/twitter/callback"] ) {
-		// we should intercept this call. Use task queue scheduler.
-		// pass the interface control back the the channel management view controller
-		progressContainerView.alpha = 0.0f;
-		progressContainerView.frame = self.view.bounds;
-		[self.view addSubview:progressContainerView];
-		// show a dark gray screen for now.
-		[[NMTaskQueueController sharedTaskQueueController] issueVerifyTwitterAccountWithURL:theURL];
-		
-		[UIView animateWithDuration:0.25f animations:^{
-			progressContainerView.alpha = 1.0f;
-		} completion:^(BOOL finished) {
-			[loadingIndicator startAnimating];
-		}];
-		
-		return NO;
+	switch (loginType) {
+		case LoginTwitterType:
+		{
+			NSLog(@"Twitter URL: %@", [theURL absoluteString]);
+			if ( [[theURL host] isEqualToString:@"api.nowbox.com"] && [[theURL path] isEqualToString:@"/auth/twitter/callback"] ) {
+				self.navigationItem.hidesBackButton = YES;
+				// we should intercept this call. Use task queue scheduler.
+				// pass the interface control back the the channel management view controller
+				progressContainerView.alpha = 0.0f;
+				progressContainerView.frame = self.view.bounds;
+				[self.view addSubview:progressContainerView];
+				// show a dark gray screen for now.
+				[[NMTaskQueueController sharedTaskQueueController] issueVerifyTwitterAccountWithURL:theURL];
+				
+				[UIView animateWithDuration:0.25f animations:^{
+					progressContainerView.alpha = 1.0f;
+				} completion:^(BOOL finished) {
+					[loadingIndicator startAnimating];
+				}];
+				
+				return NO;
+			}
+			break;
+		}
+			
+		case LoginFacebookType:
+		{
+			break;
+		}
+			
+		default:
+			break;
 	}
 	return YES;
 }
