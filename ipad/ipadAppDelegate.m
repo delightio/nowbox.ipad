d6ef0103c4986a2c775eff3643da4a456ad13aaa
Chris Haugli <chris@nowbox.com>
2011-10-25 13:01:11 -0700

    Avoid showing tooltip when app is in launch mode [#580].

--- ipad/ipadAppDelegate.m
+++ ipad/ipadAppDelegate.m
@@ -123,8 +123,6 @@ NSInteger NM_LAST_CHANNEL_ID;
 	[self.window makeKeyAndVisible];
 	
 	[[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:NULL];
-
-    [[ToolTipController sharedToolTipController] startTimer];
     
     return YES;
 }
