c3c4e1ab3e125d392eb7d96bc464060993431ae4
Chris Haugli <chris@nowbox.com>
2012-02-09 16:02:08 -0800

    Update Crittercism to v3.0RC1.

--- Libraries/CrittercismSDK/Crittercism.h
+++ Libraries/CrittercismSDK/Crittercism.h
@@ -17,61 +17,61 @@
 
 @protocol CrittercismDelegate <NSObject>
 
-@required
-// Method that is called when user clicks the close button to close the crittercism modal view
+@optional
 -(void) crittercismDidClose;
 -(void) crittercismDidCrashOnLastLoad;
 @end
 
-
-
 @interface Crittercism : NSObject {
 	NSMutableData *responseData;
-	NSObject *voteDisplayer;
+	CFMutableDictionaryRef *connectionToInfoMapping;
+	id <CrittercismDelegate> delegate;
+    BOOL didCrashOnLastLoad;
+    
+    // Support Forum
+    NSObject *voteDisplayer;
 	CrittercismViewController *crittercismViewController;
 	NSArray *feedbackArray;
-	CFMutableDictionaryRef *connectionToInfoMapping;
-	id <CrittercismDelegate> delegate;    
-    BOOL didCrashOnLastLoad;
 }
 
 @property(retain) id <CrittercismDelegate> delegate;
+@property(assign) BOOL didCrashOnLastLoad;
 @property(nonatomic, retain) CrittercismViewController *crittercismViewController;
 @property(nonatomic, retain) NSObject *voteDisplayer;
 @property(retain) NSArray *feedbackArray;
-@property(assign) BOOL didCrashOnLastLoad;
 
 + (Crittercism*)sharedInstance;
-+ (void)initWithAppID:(NSString *)_app_id andKey:(NSString *)_keyStr andSecret:(NSString *)_secretStr;
-+ (void)initWithAppID:(NSString *)_app_id andKey:(NSString *)_keyStr andSecret:(NSString *)_secretStr andMainViewController:(UIViewController *)_mainView;
-+ (void)showCrittercism;
-+ (void)showCrittercism:(UIViewController *)_mainViewController;
-+ (NSString *)getAppID;
-+ (NSString *)getKey;
-+ (NSString *)getSecret;
-+ (void)configurePushNotification:(NSData *) deviceToken;
-+ (void)setAge:(int)age;
-+ (void)setGender:(NSString *)gender;
-+ (void)setUsername:(NSString *)username;
-+ (void)setEmail:(NSString *)email;
-+ (void)setValue:(NSString *)value forKey:(NSString *)key;
++ (void) initWithAppID:(NSString *)_app_id andKey:(NSString *)_keyStr andSecret:(NSString *)_secretStr;
++ (void) initWithAppID:(NSString *)_app_id andKey:(NSString *)_keyStr andSecret:(NSString *)_secretStr andMainViewController:(UIViewController *)_mainViewController;
++ (NSString *) getAppID;
++ (NSString *) getKey;
++ (NSString *) getSecret;
++ (void) configurePushNotification:(NSData *) deviceToken;
++ (void) setAge:(int)age;
++ (void) setGender:(NSString *)gender;
++ (void) setUsername:(NSString *)username;
++ (void) setEmail:(NSString *)email;
++ (void) setValue:(NSString *)value forKey:(NSString *)key;
 + (int) getCurrentOrientation;
 + (void) setCurrentOrientation: (int)_orientation;
 + (void) logEvent:(NSString *)_eventName andEventDict:(NSDictionary *)_dict;
 + (void) leaveBreadcrumb:(NSString *)breadcrumb;
-+ (NSString *) getGMTDateStringWithFormat:(NSString *)format;
--(NSString *)applicationDocumentsDirectory;
--(CrittercismViewController *) getCrittercism;
--(UIViewController *) getMainViewController;
--(void) setTintRed:(float) _red green:(float)_green blue:(float)_blue;
--(void) hideCrittercism;
--(void) setDisplayer:(NSObject *)_displayer;
--(void) updateVotes;
--(void) addVote:(NSString *)_eventName;
--(void) addVote;
--(int) getVotes;
--(void) setVotes:(int) _numVotes;
--(void) setNavTitle:(NSString *)_title;
--(NSString *) getNavTitle;
--(void) addGradient:(UIButton *) _button;
++ (void) setOptOutStatus: (BOOL) _optOutStatus;
+
+// Support Forum
+//+ (void)showCrittercism;
+//+ (void)showCrittercism:(UIViewController *)_mainViewController;
+//-(CrittercismViewController *) getCrittercism;
+//-(UIViewController *) getMainViewController;
+//-(void) setTintRed:(float) _red green:(float)_green blue:(float)_blue;
+//-(void) hideCrittercism;
+//-(void) setDisplayer:(NSObject *)_displayer;
+//-(void) updateVotes;
+//-(void) addVote:(NSString *)_eventName;
+//-(void) addVote;
+//-(int) getVotes;
+//-(void) setVotes:(int) _numVotes;
+//-(void) setNavTitle:(NSString *)_title;
+//-(NSString *) getNavTitle;
+//-(void) addGradient:(UIButton *) _button;
 @end
