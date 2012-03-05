af431b54cb756a0ce293aaf1df3794aad19aadf2
Chris Haugli <chris@nowbox.com>
2012-03-05 14:37:16 -0800

    Show 'no comments' view for every mention without comments.

--- iphone/BuzzView.h
+++ iphone/BuzzView.h
@@ -13,17 +13,19 @@
 
 @interface BuzzView : UIView {
     NSMutableArray *commentScrollViews;
-    NSMutableArray *commentViews;
     NSMutableArray *actionButtonViews;
+    NSMutableArray *commentViews;
+    NSMutableArray *noCommentViews;
+    
+    BOOL currentMentionIsLiked;
 }
 
 @property (nonatomic, retain) IBOutlet UIView *contentView;
 @property (nonatomic, retain) IBOutlet UIScrollView *mentionsScrollView;
-@property (nonatomic, retain) IBOutlet UIView *noCommentsView;
-@property (nonatomic, retain) IBOutlet UILabel *noCommentsLabel;
 @property (nonatomic, assign) BOOL showsActionButtons;
 @property (nonatomic, assign) IBOutlet id<BuzzViewDelegate> delegate;
 
++ (UIView *)noCommentsViewWithFrame:(CGRect)frame;
 - (void)addMentionLiked:(BOOL)liked;
 - (BuzzCommentView *)addCommentWithText:(NSString *)text username:(NSString *)username;
 - (void)doneAdding;
